e0432c3df7913d5bb29a57de18dfa4284856858f
Antonija K <antonija.kasum@gmail.com>
2019-05-02 21:49:05 +0300

    Responsiveness

--- src/App.js
+++ src/App.js
@@ -41,119 +41,111 @@ class App extends Component {
     } = this.props;
     return (
       <div className="App">
-        {window.innerWidth < 1200 ? (
-          <div className="desktop">
-            <h1>Label Maker</h1>
-            <p>This app can be used on desktop only. </p>
-          </div>
-        ) : (
-          <>
-            <form className="form">
-              <div className="form__heading">
-                <h1 className="form__heading__h1">Label Maker</h1>
-
-                <input
-                  name="title"
-                  type="text"
-                  id="form-title"
-                  value={title}
-                  onChange={handleChange}
-                  className="form__heading__title"
-                  placeholder="Main title"
-                />
-
-                <input
-                  name="description"
-                  type="text"
-                  id="description"
-                  className="form__heading__description"
-                  placeholder="Main description*"
-                  value={description}
-                  onChange={handleChange}
-                />
-              </div>
-
-              <div className="form__parts">
-                {ingredients.map((part, i) => (
-                  <IngredientsSelector key={part.id} part={part} />
-                ))}
-              </div>
-              <AllergensContainer />
-              <div className="container form__package">
-                <div className="form__package__weight">
-                  <small className="form__package__weight__message">
-                    {ingredientsValidationErrors.weight}
-                  </small>
-
-                  <label className="form__package__weight__label">
-                    <span>Net.weight:</span>
-                    <input
-                      name="weight"
-                      type="text"
-                      placeholder="100"
-                      className="form__package__weight__input"
-                      value={weight}
-                      onChange={handleChange}
-                      onBlur={validate}
-                      min="0"
-                    />
-                  </label>
-                  <CustomDropdown />
-                </div>
-
-                <div className="form__package__date">
-                  <small className="form__package__date__message">
-                    {ingredientsValidationErrors.date}
-                  </small>
-                  <label className="form__package__date__label">
-                    <span>Best before:</span>
-                    <input
-                      name="date"
-                      type="date"
-                      className="form__package__date__input"
-                      value={date}
-                      onChange={handleChange}
-                      onBlur={validate}
-                      min={today}
-                    />
-                  </label>
-                </div>
-              </div>
-              <Company
-                use="producer"
-                company={producer}
-                companyList={producersList}
-                companyVisible={producersVisible}
-                errorMessage={companiesValidationErrors.producer}
-                title="Producer"
-              />
-              <Company
-                use="distributor"
-                company={distributor}
-                companyList={distributorsList}
-                companyVisible={distributorsVisible}
-                errorMessage={companiesValidationErrors.distributor}
-                title="Importer/Distributer"
-              />
-              <div className="container country-select">
-                <span className="country-select__label">
-                  Country of origin:
-                </span>
-                <select value={country} onChange={selectCountry}>
-                  <option>EU</option>
-                  {countries.length > 0 &&
-                    countries.map((country, i) => (
-                      <option key={i + 1} value={country.countryName}>
-                        {country.countryName}
-                      </option>
-                    ))}
-                </select>
-              </div>
-              <NutritionalValues />
-            </form>
-            <LabelPreview />
-          </>
+        {window.innerWidth < 800 && (
+          <p className="notification">For better experience, open on desktop</p>
         )}
+        <form className="form">
+          <div className="form__heading">
+            <h1 className="form__heading__h1">Label Maker</h1>
+
+            <input
+              name="title"
+              type="text"
+              id="form-title"
+              value={title}
+              onChange={handleChange}
+              className="form__heading__title"
+              placeholder="Main title"
+            />
+
+            <input
+              name="description"
+              type="text"
+              id="description"
+              className="form__heading__description"
+              placeholder="Main description*"
+              value={description}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="form__parts">
+            {ingredients.map((part, i) => (
+              <IngredientsSelector key={part.id} part={part} />
+            ))}
+          </div>
+          <AllergensContainer />
+          <div className="container form__package">
+            <div className="form__package__weight">
+              <small className="form__package__weight__message">
+                {ingredientsValidationErrors.weight}
+              </small>
+
+              <label className="form__package__weight__label">
+                <span>Net.weight:</span>
+                <input
+                  name="weight"
+                  type="text"
+                  placeholder="100"
+                  className="form__package__weight__input"
+                  value={weight}
+                  onChange={handleChange}
+                  onBlur={validate}
+                  min="0"
+                />
+              </label>
+              <CustomDropdown />
+            </div>
+
+            <div className="form__package__date">
+              <small className="form__package__date__message">
+                {ingredientsValidationErrors.date}
+              </small>
+              <label className="form__package__date__label">
+                <span>Best before:</span>
+                <input
+                  name="date"
+                  type="date"
+                  className="form__package__date__input"
+                  value={date}
+                  onChange={handleChange}
+                  onBlur={validate}
+                  min={today}
+                />
+              </label>
+            </div>
+          </div>
+          <Company
+            use="producer"
+            company={producer}
+            companyList={producersList}
+            companyVisible={producersVisible}
+            errorMessage={companiesValidationErrors.producer}
+            title="Producer"
+          />
+          <Company
+            use="distributor"
+            company={distributor}
+            companyList={distributorsList}
+            companyVisible={distributorsVisible}
+            errorMessage={companiesValidationErrors.distributor}
+            title="Importer/Distributer"
+          />
+          <div className="container country-select">
+            <span className="country-select__label">Country of origin:</span>
+            <select value={country} onChange={selectCountry}>
+              <option>EU</option>
+              {countries.length > 0 &&
+                countries.map((country, i) => (
+                  <option key={i + 1} value={country.countryName}>
+                    {country.countryName}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <NutritionalValues />
+        </form>
+        <LabelPreview />
       </div>
     );
   }
