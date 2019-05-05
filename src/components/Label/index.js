0674d31ac78bbc8a0bd8df912115bdf13e6f2e26
Antonija K <antonija.kasum@gmail.com>
2019-05-06 00:07:51 +0300

    pdf image ratio

--- src/components/Label/index.js
+++ src/components/Label/index.js
@@ -1,33 +1,168 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions/actions';
+import { Link } from 'react-router-dom';
 
 import LabelPreview from '../LabelPreview/index';
 
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 
-function getPDF() {
-  let element = document.getElementById('stickers');
-  html2canvas(element, { scale: 3 }).then(function(canvas) {
-    var img = canvas.toDataURL('image/png');
-    var doc = new jsPDF('l', 'mm', 'a4');
-    doc.addImage(img, 'JPEG', 1, 1, 290, 250);
-    doc.save('test.pdf');
-  });
-}
-
-const Label = () => {
+const Label = ({ title }) => {
+  const getPDF = () => {
+    let element = document.getElementById('stickers');
+    html2canvas(element, { scale: 3 }).then(function(canvas) {
+      var img = canvas.toDataURL('image/png');
+      var doc = new jsPDF('l', 'mm', 'a4');
+      doc.addImage(img, 'JPEG', 0, 0, 297, 210);
+      doc.save(`${title}.pdf`);
+    });
+  };
   return (
-    <div className="form-container" style={{ width: '100%' }}>
-      <div id="stickers" style={{ width: '1100px', display: 'flex' }}>
-        <LabelPreview />
-        <LabelPreview />
-        <LabelPreview />
-        <LabelPreview />
-        <LabelPreview />
+    <div
+      className="form-container"
+      style={{
+        width: '100%',
+        padding: '1rem',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <div
+        id="stickers"
+        style={{
+          padding: '1rem',
+          width: '848.57px',
+          height: '600px',
+          display: 'grid',
+          overflow: 'hidden',
+          border: '1px solid black',
+          margin: '0 auto',
+          gridTemplateColumns: 'repeat(7, 1fr)',
+          gridTemplateRows: 'repeat(3, 1fr)'
+        }}
+      >
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+          <div
+            style={{
+              width: '3rem',
+              height: '3rem',
+              background: 'red',
+              borderRadius: '50%'
+            }}
+          />
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <h4>{title}</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet this is dummy text</p>
+        </div>
       </div>
-      <button onClick={() => getPDF()}>Print Label</button>
+      <button className="form__submit__button" onClick={() => getPDF()}>
+        Print Label
+      </button>
+      <Link to="/" className="form__submit__button">
+        Back
+      </Link>
     </div>
   );
 };
